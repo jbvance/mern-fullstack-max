0d9bb27a08a960881148ab27f2d8d179b5619307
Jason Vance <jvance@NSLLP.local>
2022-03-23 10:08:19 -0500

    add navigation and side drawer

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Card from '../../shared/components/UIElements/Card';
 import UserItem from './UserItem';
-
 import './UsersList.css';
 
 const UsersList = ({ items }) => {
   if (!items || items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found</h2>
+        <Card>
+          <h2>No users found</h2>
+        </Card>
       </div>
     );
   }
