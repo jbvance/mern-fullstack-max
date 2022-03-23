a2c7151c01cce345a8d4c69cf014a770e21fa381
Jason Vance <jvance@NSLLP.local>
2022-03-23 14:53:56 -0500

    add custom form inputs and validations

---
+++ src/shared/components/FormElements/Input.js
@@ -0,0 +1,91 @@
+import React, { useReducer, useEffect } from 'react';
+import { validate } from '../../util/validators';
+import './Input.css';
+
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.val,
+        isValid: validate(action.val, action.validators)
+      };
+    case 'TOUCH':
+      return {
+        ...state,
+        isTouched: true
+      };
+    default:
+      return state;
+  }
+};
+
+const Input = ({
+  element,
+  type,
+  id,
+  label,
+  placeholder,
+  rows,
+  errorText,
+  validators,
+  onInput
+}) => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: '',
+    isTouched: false,
+    isValid: false
+  });
+
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, onInput, value, isValid]);
+
+  const changeHandler = (event) => {
+    dispatch({
+      type: 'CHANGE',
+      val: event.target.value,
+      validators: validators
+    });
+  };
+
+  const touchHandler = () => {
+    dispatch({ type: 'TOUCH' });
+  };
+
+  const inputElement =
+    element === 'input' ? (
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        onChange={changeHandler}
+        value={inputState.value}
+        onBlur={touchHandler}
+      />
+    ) : (
+      <textarea
+        id={id}
+        rows={rows || 3}
+        onChange={changeHandler}
+        value={inputState.value}
+        onBlur={touchHandler}
+      />
+    );
+
+  return (
+    <div
+      className={`form-control ${
+        !inputState.isValid && inputState.isTouched && 'form-control--invalid'
+      }`}
+    >
+      <label htmlFor={id}>{label}</label>
+      {inputElement}
+      {!inputState.isValid && inputState.isTouched && <p>{errorText}</p>}
+    </div>
+  );
+};
+
+export default Input;
